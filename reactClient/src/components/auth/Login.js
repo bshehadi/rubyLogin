a81a198236ae3c452a0dcdbeb24b7520f69e45fd
Baggio <shehadibaggio@gmail.com>
2019-09-30 11:49:07 -0500

    added a login component

---
+++ reactClient/src/components/auth/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    loginErrors: ""
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    axios
+      .post(
+        "http://localhost:3001/sessions",
+        {
+          user: {
+            email: email,
+            password: password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.logged_in) {
+          this.props.handleSuccessfulAuth(response.data);
+        }
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
